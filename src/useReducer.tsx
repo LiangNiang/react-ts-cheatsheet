cdc5a4e86593ff3a68dc2fe2e3e77b303d462296
Liang <liangniangbaby@gmail.com>
2022-09-15 16:28:39 +0800

    :recycle: useReducer 类型定义不再依赖 redux

--- src/useReducer.tsx
+++ src/useReducer.tsx
@@ -1,5 +1,4 @@
-import { useReducer } from 'react'
-import { Reducer } from 'redux'
+import { useReducer, Reducer } from 'react'
 
 const initialState = {
   count: 0,
@@ -9,7 +8,7 @@ type ActionType =
   | { type: 'increment'; payload: number }
   | { type: 'decrement'; payload: string };
 
-const reducer = (state: typeof initialState, action: ActionType) => {
+const reducer: Reducer<typeof initialState, ActionType> = (state, action) => {
   switch (action.type) {
   case 'increment':
     return { count: state.count + action.payload }
@@ -20,13 +19,6 @@ const reducer = (state: typeof initialState, action: ActionType) => {
   }
 }
 
-/** 如果项目中用了 redux，可以方便的使用 redux 中的 Reducer 来指定 reducer 的类型，这样可以正确推断返回值的类型 */
-const reducer2: Reducer<typeof initialState, ActionType> = () => {
-  return {
-    count: 123
-  }
-}
-
 const UseReducerExample = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
   return (
@@ -34,8 +26,20 @@ const UseReducerExample = () => {
       <h1>useReducer Example</h1>
       <h2>Count: {state.count}</h2>
       <p>
-        <button onClick={() => { dispatch({ type: 'increment', payload: 1 }) }}>Add count</button>
-        <button onClick={() => { dispatch({ type: 'decrement', payload: '2' }) }}>Reduce count</button>
+        <button
+          onClick={() => {
+            dispatch({ type: 'increment', payload: 1 })
+          }}
+        >
+          Add count
+        </button>
+        <button
+          onClick={() => {
+            dispatch({ type: 'decrement', payload: '2' })
+          }}
+        >
+          Reduce count
+        </button>
       </p>
     </div>
   )
